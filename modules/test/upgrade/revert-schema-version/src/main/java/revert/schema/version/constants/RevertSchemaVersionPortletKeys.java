de1554b0d6ab9fe19dbc75285405120919033a38
Ginson Ren <ginson.ren@liferay.com>
2019-02-22 13:51:29 +0800

    autoSF

--- modules/test/upgrade/revert-schema-version/src/main/java/revert/schema/version/constants/RevertSchemaVersionPortletKeys.java
+++ modules/test/upgrade/revert-schema-version/src/main/java/revert/schema/version/constants/RevertSchemaVersionPortletKeys.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package revert.schema.version.constants;
 
 /**
@@ -5,6 +19,6 @@ package revert.schema.version.constants;
  */
 public class RevertSchemaVersionPortletKeys {
 
-	public static final String RevertSchemaVersion = "revertschemaversion";
+	public static final String REVERT_SCHEMA_VERSION = "revertschemaversion";
 
 }
