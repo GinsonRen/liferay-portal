ebd593a78beb512d86fded7959570ac8536781f0
GinsonRen <ginson.ren@liferay.com>
2022-03-28 15:15:33 +0800

    Revert "LPS-150150 Port over unit test."
    This reverts commit c2777fd3b0fd4282983a4a964e17a4839c53c16e.

--- portal-impl/test/unit/com/liferay/portal/security/pwd/CompositePasswordEncryptorTest.java
+++ portal-impl/test/unit/com/liferay/portal/security/pwd/CompositePasswordEncryptorTest.java
@@ -12,28 +12,26 @@
  * details.
  */
 
-package com.liferay.portal.security.password.encryptor.internal;
+package com.liferay.portal.security.pwd;
 
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.security.pwd.PasswordEncryptor;
 import com.liferay.portal.kernel.security.pwd.PasswordEncryptorUtil;
-import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.util.DigesterUtil;
-import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.portal.util.DigesterImpl;
 import com.liferay.portal.util.PropsValues;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-
-import org.osgi.framework.BundleContext;
 
 /**
  * @author Tomas Polesovsky
@@ -51,70 +49,57 @@ public class CompositePasswordEncryptorTest {
 
 		digesterUtil.setDigester(new DigesterImpl());
 
-		BundleContext bundleContext = SystemBundleUtil.getBundleContext();
+		PasswordEncryptorUtil passwordEncryptorUtil =
+			new PasswordEncryptorUtil();
 
 		CompositePasswordEncryptor compositePasswordEncryptor =
 			new CompositePasswordEncryptor();
 
-		ReflectionTestUtil.invoke(
-			compositePasswordEncryptor, "activate",
-			new Class<?>[] {BundleContext.class}, bundleContext);
+		compositePasswordEncryptor.setDefaultPasswordEncryptor(
+			new DefaultPasswordEncryptor());
 
-		bundleContext.registerService(
-			PasswordEncryptor.class, compositePasswordEncryptor,
-			MapUtil.singletonDictionary("composite", "true"));
+		List<PasswordEncryptor> passwordEncryptors = new ArrayList<>();
 
-		bundleContext.registerService(
-			PasswordEncryptor.class, new DefaultPasswordEncryptor(),
-			MapUtil.singletonDictionary(
-				"type", PasswordEncryptor.TYPE_DEFAULT));
-		bundleContext.registerService(
-			PasswordEncryptor.class, new BCryptPasswordEncryptor(),
-			MapUtil.singletonDictionary("type", PasswordEncryptor.TYPE_BCRYPT));
-		bundleContext.registerService(
-			PasswordEncryptor.class, new CryptPasswordEncryptor(),
-			MapUtil.singletonDictionary(
-				"type", PasswordEncryptor.TYPE_UFC_CRYPT));
-		bundleContext.registerService(
-			PasswordEncryptor.class, new NullPasswordEncryptor(),
-			MapUtil.singletonDictionary("type", PasswordEncryptor.TYPE_NONE));
-		bundleContext.registerService(
-			PasswordEncryptor.class, new PBKDF2PasswordEncryptor(),
-			MapUtil.singletonDictionary("type", PasswordEncryptor.TYPE_PBKDF2));
-		bundleContext.registerService(
-			PasswordEncryptor.class, new SSHAPasswordEncryptor(),
-			MapUtil.singletonDictionary("type", PasswordEncryptor.TYPE_SSHA));
+		passwordEncryptors.add(new BCryptPasswordEncryptor());
+		passwordEncryptors.add(new CryptPasswordEncryptor());
+		passwordEncryptors.add(new NullPasswordEncryptor());
+		passwordEncryptors.add(new PBKDF2PasswordEncryptor());
+		passwordEncryptors.add(new SSHAPasswordEncryptor());
+
+		compositePasswordEncryptor.setPasswordEncryptors(passwordEncryptors);
+
+		passwordEncryptorUtil.setPasswordEncryptor(compositePasswordEncryptor);
 	}
 
 	@Test
 	public void testEncryptBCrypt() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_BCRYPT, "password",
+			PasswordEncryptorUtil.TYPE_BCRYPT, "password",
 			"$2a$10$/ST7LsB.7AAHsn/tlK6hr.nudQaBbJhPX9KfRSSzsn.1ij45lVzaK",
-			PasswordEncryptor.TYPE_BCRYPT);
+			PasswordEncryptorUtil.TYPE_BCRYPT);
 	}
 
 	@Test
 	public void testEncryptBCryptWith10Rounds() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_BCRYPT + "/10", "password",
+			PasswordEncryptorUtil.TYPE_BCRYPT + "/10", "password",
 			"$2a$10$JX0uYSs6pSrp05TlQxkmz.hkKGK6Av.KkNCzAYOFugO3qxjAiZleO",
-			PasswordEncryptor.TYPE_BCRYPT);
+			PasswordEncryptorUtil.TYPE_BCRYPT);
 	}
 
 	@Test
 	public void testEncryptBCryptWith12Rounds() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_BCRYPT + "/12", "password",
+			PasswordEncryptorUtil.TYPE_BCRYPT + "/12", "password",
 			"$2a$12$2dD/NrqCEBlVgFEkkFCbzOll2a9vrdl8tTTqGosm26wJK1eCtsjnO",
-			PasswordEncryptor.TYPE_BCRYPT);
+			PasswordEncryptorUtil.TYPE_BCRYPT);
 	}
 
 	@Test
 	public void testEncryptCRYPT() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_UFC_CRYPT, "password", "SNbUMVY9kKQpY",
-			PasswordEncryptor.TYPE_UFC_CRYPT);
+			PasswordEncryptorUtil.TYPE_UFC_CRYPT, "password", "SNbUMVY9kKQpY",
+			PasswordEncryptorUtil.TYPE_UFC_CRYPT);
 	}
 
 	@Test
@@ -140,68 +125,68 @@ public class CompositePasswordEncryptorTest {
 			StringPool.BLANK, StringPool.BLANK, StringPool.BLANK);
 
 		testEncryptFailure(
-			PasswordEncryptor.TYPE_SHA, "password",
+			PasswordEncryptorUtil.TYPE_SHA, "password",
 			"W6ph5Mm5Pz8GgiULbPgzG37mj9g=");
 	}
 
 	@Test
 	public void testEncryptMD2() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_MD2, "password", "8DiBqIxuORNfDsxg79YJuQ==",
-			PasswordEncryptor.TYPE_MD2);
+			PasswordEncryptorUtil.TYPE_MD2, "password",
+			"8DiBqIxuORNfDsxg79YJuQ==", PasswordEncryptorUtil.TYPE_MD2);
 	}
 
 	@Test
 	public void testEncryptMD5() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_MD5, "password", "X03MO1qnZdYdgyfeuILPmQ==",
-			PasswordEncryptor.TYPE_MD5);
+			PasswordEncryptorUtil.TYPE_MD5, "password",
+			"X03MO1qnZdYdgyfeuILPmQ==", PasswordEncryptorUtil.TYPE_MD5);
 	}
 
 	@Test
 	public void testEncryptNONE() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_NONE, "password", "password",
-			PasswordEncryptor.TYPE_NONE);
+			PasswordEncryptorUtil.TYPE_NONE, "password", "password",
+			PasswordEncryptorUtil.TYPE_NONE);
 	}
 
 	@Test
 	public void testEncryptPBKDF2() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_PBKDF2 + "WithHmacSHA1", "password",
+			PasswordEncryptorUtil.TYPE_PBKDF2 + "WithHmacSHA1", "password",
 			"AAAAoAAB9ADJZ16OuMAPPHe2CUbP0HPyXvagoKHumh7iHU3c",
-			PasswordEncryptor.TYPE_PBKDF2 + "WithHmacSHA1");
+			PasswordEncryptorUtil.TYPE_PBKDF2 + "WithHmacSHA1");
 	}
 
 	@Test
 	public void testEncryptPBKDF2With50000Rounds() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_PBKDF2 + "WithHmacSHA1/50000", "password",
-			"AAAAoAAAw1B+jxO3UiVsWdBk4B9xGd/Ko3GKHW2afYhuit49",
-			PasswordEncryptor.TYPE_PBKDF2 + "WithHmacSHA1");
+			PasswordEncryptorUtil.TYPE_PBKDF2 + "WithHmacSHA1/50000",
+			"password", "AAAAoAAAw1B+jxO3UiVsWdBk4B9xGd/Ko3GKHW2afYhuit49",
+			PasswordEncryptorUtil.TYPE_PBKDF2 + "WithHmacSHA1");
 	}
 
 	@Test
 	public void testEncryptPBKDF2With50000RoundsAnd128Key() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_PBKDF2 + "WithHmacSHA1/128/50000",
+			PasswordEncryptorUtil.TYPE_PBKDF2 + "WithHmacSHA1/128/50000",
 			"password", "AAAAoAAAw1AbW1e1Str9wSLWIX5X9swLn+j5/5+m6auSPdva",
-			PasswordEncryptor.TYPE_PBKDF2 + "WithHmacSHA1");
+			PasswordEncryptorUtil.TYPE_PBKDF2 + "WithHmacSHA1");
 	}
 
 	@Test
 	public void testEncryptPBKDF2With720000RoundsAnd128Key() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_PBKDF2 + "WithHmacSHA1/128/720000",
+			PasswordEncryptorUtil.TYPE_PBKDF2 + "WithHmacSHA1/128/720000",
 			"password", "AAAAoAAB9ADyaBP3fTtsBh8YlRn1CU7VLYR/mnH7ADMNMz2o",
-			PasswordEncryptor.TYPE_PBKDF2 + "WithHmacSHA1");
+			PasswordEncryptorUtil.TYPE_PBKDF2 + "WithHmacSHA1");
 	}
 
 	@Test
 	public void testEncryptSHA() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_SHA, "password",
-			"W6ph5Mm5Pz8GgiULbPgzG37mj9g=", PasswordEncryptor.TYPE_SHA);
+			PasswordEncryptorUtil.TYPE_SHA, "password",
+			"W6ph5Mm5Pz8GgiULbPgzG37mj9g=", PasswordEncryptorUtil.TYPE_SHA);
 	}
 
 	@Test
@@ -212,32 +197,32 @@ public class CompositePasswordEncryptorTest {
 	@Test
 	public void testEncryptSHA256() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_SHA_256, "password",
+			PasswordEncryptorUtil.TYPE_SHA_256, "password",
 			"XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=",
-			PasswordEncryptor.TYPE_SHA_256);
+			PasswordEncryptorUtil.TYPE_SHA_256);
 	}
 
 	@Test
 	public void testEncryptSHA384() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_SHA_384, "password",
+			PasswordEncryptorUtil.TYPE_SHA_384, "password",
 			"qLZLq9CsqRpZvbt3YbQh1PK7OCgNOnW6DyHyvrxFWD1EbFmGYMlM5oDEfRnDB4On",
-			PasswordEncryptor.TYPE_SHA_384);
+			PasswordEncryptorUtil.TYPE_SHA_384);
 	}
 
 	@Test
 	public void testEncryptSSHA() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_SSHA, "password",
+			PasswordEncryptorUtil.TYPE_SSHA, "password",
 			"2EWEKeVpSdd79PkTX5vaGXH5uQ028Smy/H1NmA==",
-			PasswordEncryptor.TYPE_SSHA);
+			PasswordEncryptorUtil.TYPE_SSHA);
 	}
 
 	@Test
 	public void testEncryptUFCCRYPT() throws Exception {
 		runTests(
-			PasswordEncryptor.TYPE_UFC_CRYPT, "password", "2lrTlR/pWPUOQ",
-			PasswordEncryptor.TYPE_UFC_CRYPT);
+			PasswordEncryptorUtil.TYPE_UFC_CRYPT, "password", "2lrTlR/pWPUOQ",
+			PasswordEncryptorUtil.TYPE_UFC_CRYPT);
 	}
 
 	protected void runTests(
