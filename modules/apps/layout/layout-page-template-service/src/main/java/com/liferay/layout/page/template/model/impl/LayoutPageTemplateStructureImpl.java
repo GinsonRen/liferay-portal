63f4fa3bff117a317e8a3ef175eba5c25c663bf2
hai.yu <hai.yu@liferay.com>
2022-04-08 12:53:59 +0800

    LPS-151077 Add cache field to reduce LayoutStructure.of() running times. For the same LayoutPageTemplateStructureRel, if it doesn't have any change, we may cache the field _layoutStructure. Because LayoutStructure.of() running costs many times.

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/model/impl/LayoutPageTemplateStructureImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/model/impl/LayoutPageTemplateStructureImpl.java
@@ -16,6 +16,7 @@ package com.liferay.layout.page.template.model.impl;
 
 import com.liferay.layout.page.template.model.LayoutPageTemplateStructureRel;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureRelLocalServiceUtil;
+import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.util.PortalUtil;
@@ -76,6 +77,56 @@ public class LayoutPageTemplateStructureImpl
 		}
 
 		return StringPool.BLANK;
+	}
+
+	@Override
+	public LayoutStructure getDefaultSegmentsExperienceLayoutStructure() {
+		LayoutPageTemplateStructureRel layoutPageTemplateStructureRel =
+			LayoutPageTemplateStructureRelLocalServiceUtil.
+				fetchLayoutPageTemplateStructureRel(
+					getLayoutPageTemplateStructureId(),
+					SegmentsExperienceLocalServiceUtil.
+						fetchDefaultSegmentsExperienceId(getPlid()));
+
+		if (layoutPageTemplateStructureRel == null) {
+			return new LayoutStructure();
+		}
+
+		return layoutPageTemplateStructureRel.getLayoutStructure();
+	}
+
+	@Override
+	public LayoutStructure getLayoutStructure(long segmentsExperienceId) {
+		LayoutPageTemplateStructureRel layoutPageTemplateStructureRel =
+			LayoutPageTemplateStructureRelLocalServiceUtil.
+				fetchLayoutPageTemplateStructureRel(
+					getLayoutPageTemplateStructureId(), segmentsExperienceId);
+
+		if (layoutPageTemplateStructureRel == null) {
+			return new LayoutStructure();
+		}
+
+		return layoutPageTemplateStructureRel.getLayoutStructure();
+	}
+
+	@Override
+	public LayoutStructure getLayoutStructure(String segmentsExperienceKey) {
+		SegmentsExperience segmentsExperience =
+			SegmentsExperienceLocalServiceUtil.fetchSegmentsExperience(
+				getGroupId(), segmentsExperienceKey,
+				PortalUtil.getClassNameId(Layout.class), getPlid());
+
+		LayoutPageTemplateStructureRel layoutPageTemplateStructureRel =
+			LayoutPageTemplateStructureRelLocalServiceUtil.
+				fetchLayoutPageTemplateStructureRel(
+					getLayoutPageTemplateStructureId(),
+					segmentsExperience.getSegmentsExperienceId());
+
+		if (layoutPageTemplateStructureRel == null) {
+			return new LayoutStructure();
+		}
+
+		return layoutPageTemplateStructureRel.getLayoutStructure();
 	}
 
 	@Override
--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/model/impl/LayoutPageTemplateStructureRelImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/model/impl/LayoutPageTemplateStructureRelImpl.java
@@ -14,9 +14,32 @@
 
 package com.liferay.layout.page.template.model.impl;
 
+import com.liferay.layout.util.structure.LayoutStructure;
+import com.liferay.portal.kernel.model.cache.CacheField;
+
 /**
  * @author Eudaldo Alonso
  */
 public class LayoutPageTemplateStructureRelImpl
 	extends LayoutPageTemplateStructureRelBaseImpl {
+
+	@Override
+	public LayoutStructure getLayoutStructure() {
+		if (_layoutStructure != null) {
+			return _layoutStructure;
+		}
+
+		_layoutStructure = LayoutStructure.of(getData());
+
+		return _layoutStructure;
+	}
+
+	@Override
+	public void setLayoutStructure(LayoutStructure layoutStructure) {
+		_layoutStructure = layoutStructure;
+	}
+
+	@CacheField(propagateToInterface = true)
+	private LayoutStructure _layoutStructure;
+
 }
