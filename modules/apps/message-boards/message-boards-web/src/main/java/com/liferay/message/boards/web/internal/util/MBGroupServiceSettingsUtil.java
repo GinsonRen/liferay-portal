dc5ab14494ece13b4bf29fb14bbc69a699e935bf
Dante Wang <dante.wang@liferay.com>
2022-04-22 15:22:50 +0800

    LPS-X-MB-1 Add MBGroupServiceSettingsUtil to get/set MBGroupServiceSettings with the request. Although there's a pattern to use *RequestHelper in both Java classes and JSPs, every time the request helper is needed, we need to create a new instance; in contrast, using util is easier, faster and more flexible for objects to be reused across different pages.

---
+++ modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/util/MBGroupServiceSettingsUtil.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.web.internal.util;
+
+import com.liferay.message.boards.settings.MBGroupServiceSettings;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Dante Wang
+ */
+public class MBGroupServiceSettingsUtil {
+
+	public static MBGroupServiceSettings getInstance(
+		HttpServletRequest httpServletRequest, long groupId)
+		throws PortalException {
+
+		MBGroupServiceSettings mbGroupServiceSettings =
+			(MBGroupServiceSettings)httpServletRequest.getAttribute(
+				MBGroupServiceSettingsUtil.class.getName());
+
+		if (mbGroupServiceSettings != null) {
+			return mbGroupServiceSettings;
+		}
+
+		mbGroupServiceSettings = MBGroupServiceSettings.getInstance(groupId);
+
+		httpServletRequest.setAttribute(
+			MBGroupServiceSettingsUtil.class.getName(), mbGroupServiceSettings);
+
+		return mbGroupServiceSettings;
+	}
+
+}
