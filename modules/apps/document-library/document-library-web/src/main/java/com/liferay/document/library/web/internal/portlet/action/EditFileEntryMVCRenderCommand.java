718e5aad813350b84dee29c11d7cb33ba915881d
hai.yu <hai.yu@liferay.com>
2022-05-25 11:54:25 +0800

    LPS-DL4 No longer needed. When adding/editing a dl, no jsp files used it. This code was added by 0ca5f2a501fd6b5ab984c897c8f8c261940183a1. Same code was removed by 3373d4fbf63af63c520831aff4bf86f12ea8e933 because when viewing one specific dl, no jsp files used it. Through checking profile, it can save 9s+ (It includes initialize SearchContainer logic).

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/EditFileEntryMVCRenderCommand.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/EditFileEntryMVCRenderCommand.java
@@ -17,7 +17,6 @@ package com.liferay.document.library.web.internal.portlet.action;
 import com.liferay.document.library.constants.DLPortletKeys;
 import com.liferay.document.library.web.internal.display.context.DLAdminDisplayContext;
 import com.liferay.document.library.web.internal.display.context.DLAdminDisplayContextProvider;
-import com.liferay.document.library.web.internal.display.context.DLAdminManagementToolbarDisplayContext;
 import com.liferay.dynamic.data.mapping.util.DDMFormValuesToMapConverter;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
@@ -65,14 +64,6 @@ public class EditFileEntryMVCRenderCommand
 
 		renderRequest.setAttribute(
 			DLAdminDisplayContext.class.getName(), dlAdminDisplayContext);
-
-		renderRequest.setAttribute(
-			DLAdminManagementToolbarDisplayContext.class.getName(),
-			_dlAdminDisplayContextProvider.
-				getDLAdminManagementToolbarDisplayContext(
-					_portal.getHttpServletRequest(renderRequest),
-					_portal.getHttpServletResponse(renderResponse),
-					dlAdminDisplayContext));
 
 		return super.render(renderRequest, renderResponse);
 	}
